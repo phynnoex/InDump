013a72e602fe443c64f004628636de85e1e1d276
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2026-03-27 20:31:19 -0400

    feat: enhance StickerImage component with transformer functionality and state management

--- inDump/src/components/sticker-image/index.tsx
+++ inDump/src/components/sticker-image/index.tsx
@@ -1,7 +1,11 @@
 import useImage from "use-image";
 import { useKonvaImage } from "../../hooks/useKonvaImage";
 import type { StickerImageProps } from "../../types/elementType";
-import { Image } from "react-konva";
+import { Image, Transformer } from "react-konva";
+import { useEffect, useRef, useState } from "react";
+import type { Konva } from "konva/lib/_FullInternals";
+import type { AppDispatch, RootState } from "../../state/store";
+import { useDispatch, useSelector } from "react-redux";
 useKonvaImage;
 
 export default function StickerImage({
@@ -11,16 +15,34 @@ export default function StickerImage({
   strokeWidth,
 }: StickerImageProps) {
   const [konvaImage] = useImage(imageSrc);
+  const trRef = useRef<Konva.Transformer | null>(null);
+  const imageRef = useRef<Konva.Image | null>(null);
+
+  const disptach = useDispatch<AppDispatch>();
+  const clicked = useSelector((state: RootState) => state.stageDeselect);
+
+  useEffect(() => {
+    useEffect(() => {
+      if (clicked && trRef.current && imageRef.current) {
+        trRef.current.nodes([imageRef.current]);
+      }
+    }, [konvaImage, clicked]);
+  }, [imageSrc]);
+
   if (!konvaImage) return;
   return (
-    <Image
-      draggable
-      opacity={opacity}
-      stroke={stroke}
-      strokeWidth={strokeWidth}
-      x={0}
-      y={0}
-      image={konvaImage}
-    />
+    <>
+      <Image
+      ref={imageRef}
+        draggable
+        opacity={opacity}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        x={0}
+        y={0}
+        image={konvaImage}
+      />
+      {clicked && <Transformer ref={trRef} />}
+    </>
   );
 }
