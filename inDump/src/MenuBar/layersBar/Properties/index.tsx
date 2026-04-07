1f5300a682bf6384b634cb017155ed18d77b75ce
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2026-04-06 22:54:18 -0400

    feat: add AppearanceProperties component for managing element appearance settings

--- inDump/src/MenuBar/layersBar/Properties/index.tsx
+++ inDump/src/MenuBar/layersBar/Properties/index.tsx
@@ -1,10 +1,11 @@
 import "./styles.scss";
 import type { elementIDType } from "../../../types/ElementIDTypeTypes";
-import type { ElementsNode } from "../../../types/elementType";
+
 import fonts from "../../../assets/fonts/fontLists";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../../state/store";
 import { setElements } from "../../../state/collage/collageSlice";
+import type { ElementsNode } from "../../../types/elementType";
 
 type selectedLayerType = {
   id: string;
@@ -18,6 +19,19 @@ type PropertiesBarProps = {
 export default function PropertiesBar({ selectedLayer }: PropertiesBarProps) {
   const Elements = useSelector((state: RootState) => state.elements.present);
   const dispatch = useDispatch<AppDispatch>();
+
+  const updateElementOpacity = <T extends ElementsNode>(
+    el: T,
+    opacity: number,
+  ): T => {
+    return {
+      ...el,
+      props: {
+        ...el.props,
+        opacity,
+      },
+    } as T;
+  };
 
   const handlePropertyChange = (
     e: React.ChangeEvent<
@@ -89,43 +103,34 @@ export default function PropertiesBar({ selectedLayer }: PropertiesBarProps) {
       dispatch(setElements(newElements));
     }
 
-    if (name === "text-opacity" && selectedLayer?.type === "text") {
-      const newElements = Elements.map((el) =>
-        el.id === selectedLayer.id && el.type === "text"
-          ? {
-              ...el,
-              props: {
-                ...el.props,
-                opacity: Number(value),
-              },
-            }
-          : el,
-      );
+    if (name === "general-opacity" && selectedLayer) {
+      const newElements = Elements.map((el) => {
+        if (el.id !== selectedLayer.id) {
+          return el;
+        }
+
+        return updateElementOpacity(el, Number(value));
+      });
       dispatch(setElements(newElements));
     }
   };
 
-  if (selectedLayer?.type === "text") {
-    const selectedElement = Elements.find(
-      (el) => el.id === selectedLayer.id && el.type === "text",
-    );
+  const selectedElement = Elements.find((el) => el.id === selectedLayer?.id);
 
-    if (!selectedElement || selectedElement.type !== "text") {
-      return null; // or fallback
-    }
-    return (
-      <div className="propertiesBar">
-        <h3>Properties</h3>
+  return (
+    <div className="propertiesBar">
+      <h3>Properties</h3>
+      <div className="appearance-properties">
         <h4>Appearance</h4>
         <div className="inputs">
-          <label htmlFor="text-opacity" className="w-full">
+          <label htmlFor="general-opacity" className="w-full">
             Opacity
             <br />
             <input
               className="input-field"
               type="number"
-              id="text-opacity"
-              name="text-opacity"
+              id="general-opacity"
+              name="general-opacity"
               placeholder="Opacity"
               min={0}
               max={1}
@@ -135,67 +140,73 @@ export default function PropertiesBar({ selectedLayer }: PropertiesBarProps) {
             />
           </label>
         </div>
-        <h4>Typography</h4>
-        <div className="inputs">
-          <label htmlFor="text-content" className="w-full">
-            Content
-            <br />
-            <input
-              className="input-field"
-              type="text"
-              id="text-content"
-              name="text-content"
-              placeholder="Text content"
-              onChange={handlePropertyChange}
-              value={selectedElement?.props.text || ""}
-            />
-          </label>
-          <label htmlFor="font-family" className="w-full">
-            Font Family
-            <br />
-            <select
-              id="font-family"
-              name="font-family"
-              className="input-field"
-              onChange={handlePropertyChange}
-              value={selectedElement?.props.font.fontFamily || ""}
-            >
-              {fonts.map((font) => (
-                <option key={font.fontFamily} value={font.fontFamily}>
-                  {font.fontFamily}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label htmlFor="text-size" className="w-half">
-            Fontsize
-            <br />
-            <input
-              className="input-field"
-              type="number"
-              id="text-size"
-              name="text-size"
-              placeholder="Text size"
-              min={12}
-              onChange={handlePropertyChange}
-              value={selectedElement?.props.size || ""}
-            />
-          </label>
-          <label htmlFor="text-color" className="w-half">
-            Text Color
-            <br />
-            <input
-              className="input-field"
-              type="color"
-              id="text-color"
-              name="text-color"
-              placeholder="Text color"
-              onChange={handlePropertyChange}
-              value={selectedElement?.props.color ?? "#000000"}
-            />
-          </label>
-        </div>
       </div>
-    );
-  }
+
+      {selectedElement?.type === "text" && (
+        <>
+          {" "}
+          <h4>Typography</h4>
+          <div className="inputs">
+            <label htmlFor="text-content" className="w-full">
+              Content
+              <br />
+              <input
+                className="input-field"
+                type="text"
+                id="text-content"
+                name="text-content"
+                placeholder="Text content"
+                onChange={handlePropertyChange}
+                value={selectedElement?.props.text ?? ""}
+              />
+            </label>
+            <label htmlFor="font-family" className="w-full">
+              Font Family
+              <br />
+              <select
+                id="font-family"
+                name="font-family"
+                className="input-field"
+                onChange={handlePropertyChange}
+                value={selectedElement?.props.font.fontFamily ?? "Select Font"}
+              >
+                {fonts.map((font) => (
+                  <option key={font.fontFamily} value={font.fontFamily}>
+                    {font.fontFamily}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label htmlFor="text-size" className="w-half">
+              Fontsize
+              <br />
+              <input
+                className="input-field"
+                type="number"
+                id="text-size"
+                name="text-size"
+                placeholder="Text size"
+                min={12}
+                onChange={handlePropertyChange}
+                value={selectedElement?.props.size ?? 12}
+              />
+            </label>
+            <label htmlFor="text-color" className="w-half">
+              Text Color
+              <br />
+              <input
+                className="input-field"
+                type="color"
+                id="text-color"
+                name="text-color"
+                placeholder="Text color"
+                onChange={handlePropertyChange}
+                value={selectedElement?.props.color ?? "#000000"}
+              />
+            </label>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
